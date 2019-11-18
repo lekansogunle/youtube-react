3481544f24f82cf1629d3891a67a29285b4be796
Olalekan Sogunle <sogunleolalekan@gmail.com>
2019-11-18 16:23:16 +0100

    init commit

---
+++ src/YoutubePlayer.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Form, Container} from 'semantic-ui-react';
+
+class YoutubePlayer extends Component{
+
+  render(){
+    return(
+      <div>
+        <Container textAlign='center' >
+          <Form size='small '>
+            <Form.Group>
+              <Form.Input placeholder='serach me' width={6} />
+              <Form.Button content='Search Videos' />
+            </Form.Group>
+          </Form>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default YoutubePlayer;
